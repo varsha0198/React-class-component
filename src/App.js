bb749375c3e2abf17db0d1e3c6112427730cb7e4
varsha0198 <varshabaghele1998@gmail.com>
2021-10-08 12:27:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Section from './components/Section';
+
+
+
+function App() {
+  return (
+    <div>
+     <h1> This is default component</h1> 
+     <hr />
+     <Header />
+     <hr />
+     <Footer />
+     <hr />
+     <Section />
+     
+    
+    </div>
+      
+  );
+}
+
+export default App;
---
+++ src/components/Section.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+class Section extends React.Component {
+
+    render(){
+        return <h1>This is Class Section Component</h1>
+    }
+} 
+
+export default Section;
